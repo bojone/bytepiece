847f7e698ecbbdfd613903167e40a35e069b351a
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2023-09-09 16:14:49 +0800

    简化代码

--- bytepiece/bytepiece.py
+++ bytepiece/bytepiece.py
@@ -269,14 +269,14 @@ class Tokenizer:
         self._automaton.make_automaton()
 
     def _tokenize(self, text):
-        routes = [[0, [0]]] + [[-np.inf, []] for _ in text]
+        routes = [[0, []]] + [[-np.inf, []] for _ in text]
         for e, (k, v) in self._automaton.iter(text):
             s, e = e - k + 1, e + 1
             score = routes[s][0] + v
             if score > routes[e][0]:
                 routes[e][0] = score
-                routes[e][1] = routes[s][1] + [e]
-        return [text[s:e] for s, e in zip(routes[-1][1], routes[-1][1][1:])]
+                routes[e][1] = routes[s][1] + [text[s:e]]
+        return routes[-1][1]
 
     def tokenize(self, text):
         return list(chain(*[self._tokenize(t) for t in normalize(text)]))
