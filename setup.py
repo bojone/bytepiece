6aff7c0e7c9389366444f0dd272e6c6ec38c3fe2
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2023-09-10 18:26:25 +0800

    Cython加速tokenize

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 #! -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+from Cython.Build import cythonize
 
 setup(
     name='bytepiece',
-    version='0.0.0',
+    version='0.2.0',
+    python_requires='>=3',
     description='Smarter Bytes-based Tokenizer',
     long_description='BytePiece: https://github.com/bojone/bytepiece',
     license='Apache License 2.0',
     url='https://github.com/bojone/bytepiece',
     author='bojone',
     author_email='bojone@spaces.ac.cn',
-    install_requires=['numpy', 'tqdm'],
-    packages=find_packages()
+    install_requires=['Cython', 'numpy', 'tqdm'],
+    packages=find_packages(),
+    ext_modules=cythonize('bytepiece/*.pyx'),
+    package_data={'bytepiece': ['*.pyx']}
 )
